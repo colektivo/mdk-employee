fb095625dc1a5b836aa1248d7f864df791da00b1
Guillermo Tirelli <g@tirel.li>
2015-09-12 18:29:38 +0200

    navigation and internalization

--- src/TimeTracking.elm
+++ src/TimeTracking.elm
@@ -10,42 +10,42 @@ import Html.Attributes  exposing (..)
 import Html.Events      exposing (onClick)
 import Signal           exposing (..)
 
-
-import Graphics.Element exposing (show)
-import Task exposing (Task, andThen)
-import SocketIO
-
 socket : Task x SocketIO.Socket
 socket = SocketIO.io "http://localhost:8001" SocketIO.defaultOptions
 
 port initial : Task x ()
 port initial = socket `andThen` SocketIO.emit "" "Hello I am a browser using websockets"
 
-
 port responses : Task x ()
 port responses = socket `andThen` SocketIO.on "completed" received.address
 
--- Model
+
+-- MODEL
 
 type alias Model =
-  { events: List String,
-    page: Int }
+  {  events: List String,
+    page: Int,
+    language: String
+  }
 
 initialModel : Model
 initialModel =
   {
     events = [],
-    page = 0
-    }
+    page = 0,
+    language = ""
+  }
 
--- actions
+
+-- ACTIONS
 
 type Action = Next
-            | Previous
-            | Reset
-            | Start String
+          | Previous
+          | Reset
+          | Start String
+          | Language String
 
--- update
+-- UPDATE
 
 update : Action -> Model -> Model
 update action log =
@@ -57,7 +57,9 @@ update action log =
     Start data ->
       { log | events <- log.events ++ [data] }
     Reset ->
-      { log | events <- [] , page <- 0 }
+      { log | events <- [], page <- 0 }
+    Language language ->
+      { log | language <- language, page <- 2}
 
 -- SIGNALS
 
@@ -81,7 +83,7 @@ model =
   foldp update initialModel allinputs
 
 
--- view
+-- VIEW
 
 previous: Address Action -> Model -> Html
 previous address model =
@@ -91,31 +93,101 @@ next: Address Action -> Model -> Html
 next address model =
   li [ onClick address Next ] [ text "Next" ]
 
-navigation: Address Action -> Model -> Html
-navigation address model =
-  let
-    links =
-      if model.page > 0 then [previous address model] else []
-    alllinks =
-      if model.page < 5 then links ++ [next address model] else links
-  in
-    ul []
-      alllinks
+-- navigation: Address Action -> Model -> Html
+-- navigation address model =
+--   let
+--     links =
+--       if model.page > 0 then [previous address model] else []
+--     alllinks =
+--       if model.page < 5 then links ++ [next address model] else links
+--   in
+--     ul []
+--       alllinks
+
+phrase: String -> String -> Html
+phrase key language =
+  case language of
+    "de" ->
+      case key of
+        "welcome" -> text "Willkommen!"
+        "place_chip" -> text "Bitte halte deine Chipkarte auf das Lesegerät um deine persönliche Auswertung zu erhalten"
+    "en" ->
+      case key of
+        "welcome" -> text "Welcome!"
+        "place_chip" -> text "Please place your chipcard on the reading device to receive your personal evaluation"
+
+content: Address Action -> Model -> Html
+content address model =
+  case model.page of
+    0 ->
+      div [ class "wrapper" ]
+      [
+        div [ class "main_item"]
+        [
+          p []
+          [ phrase "place_chip" "de" ]
+        ],
+        div [ class "main_item"]
+        [
+          p []
+          [ phrase "place_chip" "en" ]
+        ],
+        a [ onClick address Next]
+        [
+          text "Next"
+        ]
+      ]
+    1 ->
+      div [ class "wrapper -second" ]
+      [
+        div [ class "main_item"]
+        [
+          p []
+          [ phrase "welcome" "de" ],
+          a [ onClick address (Language "de"), class "big_button", href "#"]
+          [
+            p []
+            [ text "Deutsch" ]
+          ]
+        ],
+        div [ class "main_item"]
+        [
+          p []
+          [ phrase "welcome" "en" ],
+          a [ onClick address (Language "en"), class "big_button", href "#"]
+          [
+            p []
+            [ text "English" ]
+          ]
+        ]
+      ]
+    2 ->
+      div [ class "wrapper -second" ]
+      [
+        div [ class "main_item"]
+        [
+          p []
+          [ phrase "welcome" model.language ]
+        ]
+      ]
 
 view : Address Action -> Model -> Html
 view address model =
   div []
-    [ navigation address model,
-      h1 [ onClick address Reset ]
-        [ text "Reset" ],
-      div []
-        (List.map (\t -> text t) model.events),
-      div []
-        [ text (toString model.page) ]
-      ]
+    [
+      -- navigation address model,
+      content address model
+      -- ,
+      -- h1 [ onClick address Reset ]
+      --   [ text "Reset" ],
+      -- div []
+      --   (List.map (\t -> text t) model.events),
+      -- div []
+      --   [ text (toString model.page) ]
+    ]
 
 
--- main
+-- MAIN
 
 main : Signal Html
 main =
