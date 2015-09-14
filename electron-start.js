a4b18ebc755e424524c58dbfdfe5183b2078af2b
Pedro Visintin <pedro.visintin@gmail.com>
2015-09-14 21:20:33 +0200

    default size for the monitor in the exhibition

--- electron-start.js
+++ electron-start.js
@@ -21,7 +21,7 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1920, height: 1080});
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
